5dd98ce9f9de795cc3f18b702543a303f393e0a4
tarendt <tarendt>
2012-10-14 20:20:14 +0000

    new smell SpeculativeGeneralityEClass added

---
+++ org.eclipse.emf.refactor.smells.ecore/src/org/eclipse/emf/refactor/smells/ecore/SpeculativeGeneralityEClass.java
@@ -0,0 +1,40 @@
+package org.eclipse.emf.refactor.smells.ecore;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.LinkedList;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.emf.ecore.EObject;
+
+import org.eclipse.emf.refactor.smells.henshin.managers.HenshinRuntimeManager;
+import org.eclipse.emf.refactor.smells.interfaces.IModelSmellFinderClass;
+
+
+public final class SpeculativeGeneralityEClass implements IModelSmellFinderClass {
+	
+	private final String transformationPath = "transformations/";
+	private final String henshinFileName = "speculative.henshin";
+
+	@Override
+	public LinkedList<LinkedList<EObject>> findSmell(EObject root) {
+		String fullPath = getFullPath(transformationPath + henshinFileName);
+		return HenshinRuntimeManager.run(root, fullPath);
+	}
+	
+	private String getFullPath(String transformationPath){
+		URL url = FileLocator.find(org.eclipse.emf.refactor.smells.ecore.Activator.getDefault().getBundle(), 
+				new Path(transformationPath), Collections.EMPTY_MAP);
+		URL fileUrl = null;
+		try {
+			fileUrl = FileLocator.toFileURL(url);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		return 	fileUrl.getPath();
+	}
+	
+}
