201e0d0446af5bb514d2baa669255f5a173ef8b5
tarendt <tarendt>
2013-01-12 14:44:29 +0100

    smell analysis button added

--- org.eclipse.emf.refactor.refactorings.ecore/src/org/eclipse/emf/refactor/refactorings/ecore/renameeclass/RefactoringWizardPage.java
+++ org.eclipse.emf.refactor.refactorings.ecore/src/org/eclipse/emf/refactor/refactorings/ecore/renameeclass/RefactoringWizardPage.java
@@ -6,6 +6,11 @@
  */
  package org.eclipse.emf.refactor.refactorings.ecore.renameeclass;
 
+import java.util.List;
+
+import org.eclipse.emf.refactor.refactoring.runtime.ltk.ui.AbstractRefactoringWizard;
+import org.eclipse.emf.refactor.refactoring.runtime.ui.IInputPageButtonCreator;
+import org.eclipse.emf.refactor.refactoring.runtime.ui.InputPageButtonLoader;
 import org.eclipse.ltk.ui.refactoring.UserInputWizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
@@ -107,6 +112,10 @@ public class RefactoringWizardPage extends
 		newNameWidget.setLayoutData(gd);
 		newNameWidget.addListener(SWT.Modify, this);
 		
+		List<IInputPageButtonCreator> buttonCreators = InputPageButtonLoader.iNSTANCE.getInputPageButtonCreatorClasses();
+		for(IInputPageButtonCreator creator : buttonCreators){
+			creator.createButton(composite, controller, (AbstractRefactoringWizard)this.getWizard());
+		}		
 		
 		setControl(composite);
 	}
